54065c0147d491bdb1e1acb5970076f6ce8e0bec
= <teiva.tesson@etu.unicaen.fr>
2023-03-03 10:38:47 +0100

    feat: a first 2d render

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct SnakeHead;
+
+const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+
+fn spawn_snake(mut commands: Commands) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: SNAKE_HEAD_COLOR,
+                ..default()
+            },
+            transform: Transform {
+                scale: Vec3::new(10.0, 10.0, 10.0),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(SnakeHead);
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_startup_system(setup_camera)
+        .add_startup_system(spawn_snake)
+        .add_plugins(DefaultPlugins)
+        .run();
 }
